5dac5d3c375c221cb9944a7680382aed80ca6a0b
scccrim <scccrim@DESKTOP-EUH4BC4>
2026-03-23 00:05:42 +0100

    Pridani menu a flowlayout panelu jak pro jednotlive tiery tak i pro serialy

--- TierListSerialu/FormTierList.Designer.cs
+++ TierListSerialu/FormTierList.Designer.cs
@@ -28,12 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormTierList";
+            menuStrip1 = new MenuStrip();
+            itemUlozit = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            toolStripMenuItem4 = new ToolStripMenuItem();
+            toolStripMenuItem5 = new ToolStripMenuItem();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            label2 = new Label();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            label3 = new Label();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            label4 = new Label();
+            flowLayoutPanel4 = new FlowLayoutPanel();
+            label5 = new Label();
+            flowLayoutPanel5 = new FlowLayoutPanel();
+            flowLayoutPanel6 = new FlowLayoutPanel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { itemUlozit, toolStripMenuItem1, toolStripMenuItem2, toolStripMenuItem3 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1904, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // itemUlozit
+            // 
+            itemUlozit.Name = "itemUlozit";
+            itemUlozit.Size = new Size(49, 20);
+            itemUlozit.Text = "Uložit";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(71, 20);
+            toolStripMenuItem1.Text = "O aplikaci";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(73, 20);
+            toolStripMenuItem2.Text = "Nápověda";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem4, toolStripMenuItem5 });
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(51, 20);
+            toolStripMenuItem3.Text = "Režim";
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new Size(108, 22);
+            toolStripMenuItem4.Text = "Tmavý";
+            // 
+            // toolStripMenuItem5
+            // 
+            toolStripMenuItem5.Name = "toolStripMenuItem5";
+            toolStripMenuItem5.Size = new Size(108, 22);
+            toolStripMenuItem5.Text = "Světlý";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(104, 116);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1023, 126);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 56F, FontStyle.Bold);
+            label1.ForeColor = Color.Lime;
+            label1.Location = new Point(14, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 100);
+            label1.TabIndex = 2;
+            label1.Text = "S";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 56F, FontStyle.Bold);
+            label2.ForeColor = Color.Cyan;
+            label2.Location = new Point(12, 258);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 100);
+            label2.TabIndex = 3;
+            label2.Text = "A";
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Location = new Point(104, 248);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(1023, 126);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 56F, FontStyle.Bold);
+            label3.ForeColor = Color.Yellow;
+            label3.Location = new Point(14, 390);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 100);
+            label3.TabIndex = 4;
+            label3.Text = "B";
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Location = new Point(104, 380);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(1023, 126);
+            flowLayoutPanel3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI", 56F, FontStyle.Bold);
+            label4.ForeColor = Color.DarkGoldenrod;
+            label4.Location = new Point(14, 528);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 100);
+            label4.TabIndex = 5;
+            label4.Text = "C";
+            // 
+            // flowLayoutPanel4
+            // 
+            flowLayoutPanel4.Location = new Point(104, 512);
+            flowLayoutPanel4.Name = "flowLayoutPanel4";
+            flowLayoutPanel4.Size = new Size(1023, 126);
+            flowLayoutPanel4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 56F, FontStyle.Bold);
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(14, 655);
+            label5.Name = "label5";
+            label5.Size = new Size(84, 100);
+            label5.TabIndex = 6;
+            label5.Text = "D";
+            // 
+            // flowLayoutPanel5
+            // 
+            flowLayoutPanel5.Location = new Point(104, 645);
+            flowLayoutPanel5.Name = "flowLayoutPanel5";
+            flowLayoutPanel5.Size = new Size(1023, 126);
+            flowLayoutPanel5.TabIndex = 5;
+            // 
+            // flowLayoutPanel6
+            // 
+            flowLayoutPanel6.Location = new Point(12, 777);
+            flowLayoutPanel6.Name = "flowLayoutPanel6";
+            flowLayoutPanel6.Size = new Size(1489, 252);
+            flowLayoutPanel6.TabIndex = 6;
+            // 
+            // FormTierList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(flowLayoutPanel6);
+            Controls.Add(flowLayoutPanel5);
+            Controls.Add(label5);
+            Controls.Add(flowLayoutPanel4);
+            Controls.Add(label4);
+            Controls.Add(flowLayoutPanel3);
+            Controls.Add(label3);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "FormTierList";
+            Text = "FormTierList";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem itemUlozit;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem toolStripMenuItem4;
+        private ToolStripMenuItem toolStripMenuItem5;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Label label3;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Label label4;
+        private FlowLayoutPanel flowLayoutPanel4;
+        private Label label5;
+        private FlowLayoutPanel flowLayoutPanel5;
+        private FlowLayoutPanel flowLayoutPanel6;
     }
 }
