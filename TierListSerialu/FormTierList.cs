906ce5dbad4900c200130a5c0782ca252930a09f
scccrim <scccrim@DESKTOP-EUH4BC4>
2026-03-29 18:26:01 +0200

    Povoleni funkce drop u vsech panelu, funkce drag, dragenter, drag drop, napojeni udalosti na jednotlive panely

--- TierListSerialu/FormTierList.cs
+++ TierListSerialu/FormTierList.cs
@@ -23,6 +23,25 @@ namespace TierListSerialu
         }
         private void formTierList_Load(object sender, EventArgs e)
         {
+            flowLayoutPanelS.AllowDrop = true;
+            flowLayoutPanelA.AllowDrop = true;
+            flowLayoutPanelB.AllowDrop = true;
+            flowLayoutPanelC.AllowDrop = true;
+            flowLayoutPanelD.AllowDrop = true;
+            flowLayoutPanelS.DragEnter += Panel_DragEnter;
+            flowLayoutPanelS.DragDrop += Panel_DragDrop;
+
+            flowLayoutPanelA.DragEnter += Panel_DragEnter;
+            flowLayoutPanelA.DragDrop += Panel_DragDrop;
+
+            flowLayoutPanelB.DragEnter += Panel_DragEnter;
+            flowLayoutPanelB.DragDrop += Panel_DragDrop;
+
+            flowLayoutPanelC.DragEnter += Panel_DragEnter;
+            flowLayoutPanelC.DragDrop += Panel_DragDrop;
+
+            flowLayoutPanelD.DragEnter += Panel_DragEnter;
+            flowLayoutPanelD.DragDrop += Panel_DragDrop;
             string cesta = Path.Combine(Application.StartupPath, "images");
 
             string[] soubory = Directory.GetFiles(cesta);
@@ -30,12 +49,39 @@ namespace TierListSerialu
             {
                 PictureBox pb = new PictureBox();
                 pb.Width = 150;
-                pb.Height = 200;
+                pb.Height = 126;
                 pb.SizeMode = PictureBoxSizeMode.Zoom;
+                pb.MouseDown += Pb_MouseDown;
                 pb.Image = Image.FromFile(soubor);
 
                 flowLayoutPanelSerialy.Controls.Add(pb);
             }
         }
+        private void Pb_MouseDown(object sender, MouseEventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            if (pb != null)
+            {
+                pb.DoDragDrop(pb, DragDropEffects.Move);
+            }
+        }
+        private void Panel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+        private void Panel_DragDrop(object sender, DragEventArgs e)
+        {
+            PictureBox pb = (PictureBox)e.Data.GetData(typeof(PictureBox));
+            FlowLayoutPanel panel = sender as FlowLayoutPanel;
+
+            if (pb != null && panel != null)
+            {
+                panel.Controls.Add(pb);
+            }
+        }
+
     }
 }
